a3ad896db5e48912a81092bfb618d40ab06c31dd
joanmelo89 <joanmelo89@gmail.com>
2017-10-02 20:05:42 -0300

    Métodos +
    Métodos +

---
+++ LP2/metodos/Q1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+
+/**
+ *
+ * @author joan-
+ */
+public class Q1 {
+    public static void main(String[] args){
+        
+        Lampada lampada = new Lampada();
+        
+        lampada.ligar();
+        
+        lampada.mostrarEstado();
+        
+        lampada.desligar();
+        
+        lampada.mostrarEstado();
+        
+        lampada.mudarEstado();
+        
+        lampada.mostrarEstado();
+    }
+}
